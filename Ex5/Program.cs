73e68ed75ac49a67bf72fcfe66a35f0169ea3add
Ana Clara <acccarneiro@sga.pucminas.br>
2025-11-01 11:35:08 -0300

    Ex5 Inacabado

--- Ex5/Program.cs
+++ Ex5/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
 
 namespace Ex5
 {
@@ -10,6 +7,25 @@ namespace Ex5
     {
         static void Main(string[] args)
         {
+            /*05. Implemente um método que receba um vetor de char como parâmetro e retorne o número de vogais que constam no vetor.*/
+
+            int num = 0;
+
+            Console.WriteLine("Quantas letras deseja informar?");
+            num = int.Parse(Console.ReadLine());
+
+            char[] letras = new char[num];
+
+            for (int i = 0; i < num; i++)
+            {
+                Console.WriteLine($"Letra {i + 1}:");
+                letras[i] = char.Parse(Console.ReadLine().ToLower());
+            }
+        }
+        static char [] Vogais ()
+        {
+            char [] vogais = { 'a', 'e', 'i', 'o','u'};
+            return vogais;
         }
     }
 }
